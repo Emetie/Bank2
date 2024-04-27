bd9b3a274c2b505531cadcfddbc4bf97bfd6d7ad
Emetie <145870917+Emetie@users.noreply.github.com>
2024-04-26 21:23:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 class Sam:
   def __init__(self):
-      self.name = "Sam"
-      self.bank_name = "Fictional Bank"
+      self.name = "Samuel"
+      self.bank_name = "Emeti's Bank"
 
   def welcome(self):
       return f"Welcome to {self.bank_name}, {self.name}! How can I help you today?"
 
   def account_balance(self, acc):
-      return f"{self.name}: Account {acc} balance: $100,000."
+      return f"{self.name}: Account {acc} balance: $10,000."
 
   def transaction_history(self, acc):
-      return f"{self.name}:Recent transactions for {acc}:\n- Deposit: +$1,000\n-WD:-$90"
+      return f"{self.name}:Recent transactions for {acc}: Deposit: +$1,000: WD:$90"
 
   def customer_support(self):
       return f"{self.name}: Customer support available 24/7. How can we assist you?"
@@ -20,3 +20,4 @@ print(sam.welcome())
 print(sam.account_balance("123456"))
 print(sam.transaction_history("123456"))
 print(sam.customer_support())
+                          
